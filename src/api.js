867fc1fd8cbcc56ad637626f1472afbf0742dc5d
DannySolano <119617205+DannySolano@users.noreply.github.com>
2025-02-12 14:08:07 -0500

    add custom host

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api'; // Cambia la URL según la dirección de tu backend.
+const API_URL = 'http://167.99.146.213/api'; // Cambia la URL según la dirección de tu backend.
 
 export const getTransactions = async () => {
     try {
